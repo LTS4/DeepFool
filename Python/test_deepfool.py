5662487e9f91c6408cd5fea13c8485f6dc54addf
Jessica Landon <15jdl4@queensu.ca>
2020-03-08 17:49:47 -0400

    Code to create adversarial examples and calculate robustness measure (still buggy tho).

--- Python/test_deepfool.py
+++ Python/test_deepfool.py
@@ -14,49 +14,115 @@ from PIL import Image
 from deepfool import deepfool
 import os
 
-net = models.resnet34(pretrained=True)
+# Number of images to perturb
+N = 100
+# List to hold L2 norms of r for all perturbed images so rho can be caluclated at the end
+r_arr = []
+# List to hold original labels
+orig_labels = []
+# List to hold perturbed labels
+pert_labels = []
+# List to hold L2 norms
+L2_norms = []
+# List of original images
+orig_imgs = []
+# Cumulative sum for rho
+rho_sum = 0
+
+# Network you're using (can change to whatever)
+net = models.googlenet(pretrained=True)
 
 # Switch to evaluation mode
 net.eval()
 
-im_orig = Image.open('test_im2.jpg')
-
-mean = [ 0.485, 0.456, 0.406 ]
-std = [ 0.229, 0.224, 0.225 ]
-
-
-# Remove the mean
-im = transforms.Compose([
-    transforms.Scale(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean = mean,
-                         std = std)])(im_orig)
-
-r, loop_i, label_orig, label_pert, pert_image = deepfool(im, net)
-
-labels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
-
-str_label_orig = labels[np.int(label_orig)].split(',')[0]
-str_label_pert = labels[np.int(label_pert)].split(',')[0]
-
-print("Original label = ", str_label_orig)
-print("Perturbed label = ", str_label_pert)
-
 def clip_tensor(A, minv, maxv):
-    A = torch.max(A, minv*torch.ones(A.shape))
-    A = torch.min(A, maxv*torch.ones(A.shape))
-    return A
+        A = torch.max(A, minv*torch.ones(A.shape))
+        A = torch.min(A, maxv*torch.ones(A.shape))
+        return A
 
-clip = lambda x: clip_tensor(x, 0, 255)
+# Get list of files in ImageNet directory (you gotta save this in DeepFool/Python to get it to work like this)
+for (root, dirs, files) in os.walk("ILSVRC2012_img_val", topdown=True):
+        sorted_files = sorted(files, key=lambda item: int(item[18:23]))
 
-tf = transforms.Compose([transforms.Normalize(mean=[0, 0, 0], std=map(lambda x: 1 / x, std)),
-                        transforms.Normalize(mean=map(lambda x: -x, mean), std=[1, 1, 1]),
-                        transforms.Lambda(clip),
-                        transforms.ToPILImage(),
-                        transforms.CenterCrop(224)])
+# Now for every image:
+for i in range(N):
+        # Something wrong with this image, this is a patch fix
+        if (sorted_files[i] != "ILSVRC2012_val_00000034.JPEG"):
+                # Open image in directory (traverse from top down)
+                orig_img = Image.open("ILSVRC2012_img_val/" + sorted_files[i])
 
-plt.figure()
-plt.imshow(tf(pert_image.cpu()[0]))
-plt.title(str_label_pert)
-plt.show()
+
+                mean = [ 0.485, 0.456, 0.406 ]
+                std = [ 0.229, 0.224, 0.225 ]
+
+                # Get vector form of image so L2 norm of image x can be calculated (See denominator of eqn 15 in DeepFool paper)
+                img_arr = np.array(orig_img)
+                img_vect = img_arr.ravel()
+                L2_norms.append(np.linalg.norm(img_vect))
+
+
+                # Remove the mean
+                im = transforms.Compose([
+                        transforms.Resize(256),
+                        transforms.CenterCrop(224),
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean = mean,
+                                 std = std)])(orig_img)
+
+                r, loop_i, label_orig, label_pert, pert_image = deepfool(im, net)
+
+                # Add L2 norm of perturbation to array (See numerator of eqn 15 in DeepFool paper)
+                r_norm = np.linalg.norm(r)
+                r_arr.append(r_norm)
+
+                # Add original labels and perturbed labels to array (just in case you need them later, not rlly using rn)
+                orig_labels.append(label_orig)
+                pert_labels.append(label_pert)
+
+                labels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
+
+                str_label_orig = labels[np.int(label_orig)].split(',')[0]
+                str_label_pert = labels[np.int(label_pert)].split(',')[0]
+
+                print("Original label = ", str_label_orig)
+                print("Perturbed label = ", str_label_pert)
+    
+                clip = lambda x: clip_tensor(x, 0, 255)
+
+                ### These commented lines were throwing errors
+
+                #tf = transforms.Compose([transforms.Normalize(mean=[0, 0, 0], std=map(lambda x: 1 / x, std)),
+                #transforms.Normalize(mean=map(lambda x: -x, mean), std=[1, 1, 1]),
+                #transforms.Lambda(clip),
+                #transforms.ToPILImage(),
+                #transforms.CenterCrop(224)])
+    
+                ### Changed it to this
+
+                tf =  transforms.Compose([transforms.Normalize(mean = [0, 0, 0],
+                                               std = [(1/0.229), (1/0.244), (1/0.255)]), transforms.Normalize(mean = [-0.485, -0.456, -0.406], std=[1,1,1]),
+                          transforms.Lambda(clip), transforms.ToPILImage(),
+                          transforms.Resize(256),
+                          transforms.CenterCrop(224)])
+
+                # Write image file to directory to hold perturbed images
+                if (os.path.exists('pert_imgs') != 1):
+                        os.mkdir('pert_imgs')
+                        tf(pert_image.cpu()[0]).save('pert_imgs/' + sorted_files[i], 'JPEG')
+    
+
+                        ## Commented this out because u probably don't want a bunch of images popping up
+        
+                        #plt.figure()
+                        #plt.imshow(tf(pert_image.cpu()[0]))
+                        #plt.title(str_label_pert)
+                        #plt.show()
+
+                        # Add to cumulative sum term to get rho (See eqn 15 in DeepFool paper)
+                        rho_sum = rho_sum + r_norm/np.linalg.norm(img_vect)
+
+# Compute average robustness (rho) for the simulation (See eqn 15 in DeepFool paper)
+rho = (1/N)*rho_sum
+print(rho)
+
+
