7b73f83ba00f92247cf9f7ae98362d131ecd74ac
Troy Giorshev <troygiorshev@gmail.com>
2020-03-10 20:18:01 -0400

    Make practice MNIST work

--- Python/train_MNIST_practice.py
+++ Python/train_MNIST_practice.py
@@ -64,8 +64,56 @@ def main():
     batch_idx, (example_data, example_targets) = next(examples)
     print(example_data.shape)
 
-    # Ok, build a network
+    # Make the NN
+    network = Net()
+    optimizer = optim.SGD(network.parameters(), lr=learning_rate,
+                      momentum=momentum)
 
+    # For nice graphs later
+    train_losses = []
+    train_counter = []
+    test_losses = []
+    test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+    # Training Function
+    def train(epoch):
+        network.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            optimizer.zero_grad()
+            output = network(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()))
+                train_losses.append(loss.item())
+                train_counter.append(
+                    (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+                torch.save(network.state_dict(), '../models/MNIST/practice/model.pth')
+                torch.save(optimizer.state_dict(), '../models/MNIST/practice/optimizer.pth')
+
+    def test():
+        network.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                output = network(data)
+                test_loss += F.nll_loss(output, target, size_average=False).item()
+                pred = output.data.max(1, keepdim=True)[1]
+                correct += pred.eq(target.data.view_as(pred)).sum()
+        test_loss /= len(test_loader.dataset)
+        test_losses.append(test_loss)
+        print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+    
+    test()
+    for epoch in range(1, n_epochs + 1):
+        train(epoch)
+        test()
 
 
 if __name__ == "__main__":
